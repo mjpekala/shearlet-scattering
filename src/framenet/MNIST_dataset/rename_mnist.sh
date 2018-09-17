54a059bc7c4825d3199dec57c3e358b88bda3c37
mike pekala <mjp@mikes-Air.home>
2018-09-16 22:41:45 -0400

    added

---
+++ src/framenet/MNIST_dataset/rename_mnist.sh
@@ -0,0 +1,5 @@
+# for some reason, the framenet codes do not use the default filenames sometimes.
+# mjp, 2018
+
+mv t10k-images-idx3-ubyte t10k-images.idx3-ubyte
+mv train-images-idx3-ubyte train-images.idx3-ubyte
